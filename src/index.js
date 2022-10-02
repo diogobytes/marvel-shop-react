13aaba075802734c255520a25796e009cc2fc676
Diogo Guedes <diogoguedes@MBP-de-cex.Home>
2022-10-02 16:08:11 +0100

    UserContext, Implementing Observable Pattern

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './';
 import App from './App';
+import { UserProvider } from './contexts/user.context';
 import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <UserProvider>
     <App />
+    </UserProvider>
     </BrowserRouter>
     
   </React.StrictMode>
